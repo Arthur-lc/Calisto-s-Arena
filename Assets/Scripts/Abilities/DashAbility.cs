871e5c9c9ff4fee36072e7dd3f0074cd3e16ad10
Arthur <costa.arthur.lima@gmail.com>
2022-03-01 02:31:26 -0300

    Project Setup and Ability Sistem

---
+++ Assets/Scripts/Abilities/DashAbility.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Dash")]
+public class DashAbility : Ability
+{
+    public float dashVelocity;
+    
+    public override void Activate(GameObject parent) {
+        MovementController movement = parent.GetComponent<MovementController>();
+        Rigidbody2D rb = parent.GetComponent<Rigidbody2D>();
+
+        rb.velocity = movement.movementInput.normalized * dashVelocity;
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float normalAcceleration = 30f;
+    [HideInInspector] public float acceleration;
+    [HideInInspector] public Vector2 movementInput;
+    public Transform mouseDir;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        acceleration = normalAcceleration;
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseDir.up = (mousePos - (Vector2)transform.position).normalized;
+    }
+
+    private void FixedUpdate() {
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementInput = movementInput.normalized;
+        rb.velocity += movementInput * acceleration * Time.fixedDeltaTime;
+    }
+}
