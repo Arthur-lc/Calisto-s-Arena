0bac1fc501ac1f0d21909d348fae4b7b438f3291
Arthur <costa.arthur.lima@gmail.com>
2022-03-04 00:46:36 -0300

    Horde System

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,8 +8,7 @@ public class Enemy : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     [SerializeField] private float speed = .5f;
-
-
+    [SerializeField] private LayerMask playerMask;
 
     private void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -21,5 +20,24 @@ public class Enemy : MonoBehaviour
         Vector2 moveDir = player.transform.position - transform.position;
         moveDir = moveDir.normalized;
         rb.velocity = moveDir * speed;
+    }
+
+    public void Spawn(float radius) {
+        transform.position = RandomPolar(radius);
+        while (Physics2D.OverlapCircle(transform.position, 1f, playerMask))
+        {
+            transform.position = RandomPolar(radius);
+        }
+    }
+
+    private Vector2 RandomPolar (float radius) {
+        /*
+            retorna um vetor com um ponto aleatorio dentro de um criculo
+            de raio radius, utilizando coordenadas polares.
+            https://www.youtube.com/watch?v=4y_nmpv-9lI
+        */
+        float theta = Random.Range(0f,2f * 3.14f);
+        float r = Random.Range(0f, radius);
+        return new Vector2(r * Mathf.Cos(theta), r * Mathf.Sin(theta));
     }
 }
--- Assets/Scripts/HordeSystem.cs
+++ Assets/Scripts/HordeSystem.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class HordeSystem : MonoBehaviour
 {
+    [System.Serializable] public struct EnemyGroup
+    {
+        public int quantity;
+        public GameObject enemy;
+    }
+
+    [System.Serializable] public class Wave
+    {
+        public EnemyGroup[] enemyGroups;
+    }
+    
     public Ability ability; //gambiarra
     public KeyCode key; //gambiarra
-    AbilityManager abilityManager;
+
+    [Tooltip("Area where enemies will spawn")]
+    public float radius;
+
+    [SerializeField] public Wave[] waves;
+    private int currentWaveNumber = 0;
+
+
+    private Wave currentWave;
+    private AbilityManager abilityManager;
 
     private void Start() {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -15,8 +37,40 @@ public class HordeSystem : MonoBehaviour
 
     void Update()
     {
+        // gabiarra
         if (Input.GetKeyDown(KeyCode.K)) {
-            abilityManager.AddNewAbility(ability, key);
+            EndWave();
+            NewWave();
         }
+        // gambiarra
+    }
+
+    private void NewWave() {
+        currentWave = waves[currentWaveNumber];
+        foreach(var enemyGroup in currentWave.enemyGroups) 
+        {
+            for (int i = 0; i < enemyGroup.quantity; i++)
+            {
+                GameObject newEnemy = Instantiate(enemyGroup.enemy);
+                newEnemy.GetComponent<Enemy>().Spawn(radius);
+
+                /*
+                Vector2 enemyPosition = RandomPolar(radius);
+                
+                newEnemy.transform.position = enemyPosition;
+                */
+            }
+        }
+        currentWaveNumber++;
+    }
+
+    private void EndWave() {
+        Debug.Log("end fase");
+        abilityManager.AddNewAbility(ability, key);
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(Vector3.zero, radius);
     }
 }
