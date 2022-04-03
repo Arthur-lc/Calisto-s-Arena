977abb87d2f332db9a2803a10c0fa4541468f771
Arthur <costa.arthur.lima@gmail.com>
2022-04-03 00:25:27 -0300

    Main Menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public Animation um;
+    public Animation dois;
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            um.Play();
+            dois.Play();
+        }
+    }
+
+    public void Play() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
